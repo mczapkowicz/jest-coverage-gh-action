adac865280a83085a80dbf6c727d72332df70b00
Michał Czapkowicz <michal.czapkowicz@spoton.com>
2023-05-24 22:01:46 +0200

    asds

--- src/utils.test.js
+++ src/utils.test.js
@@ -4,3 +4,8 @@ test('convert', () => {
     const a = convertToCurrency(12)
     expect(a).toEqual("€ 12,00")
 });
+
+test('convert2' , () => {
+    const a = convertToCurrency(12, "PLN")
+    expect(a).toEqual("PLN 12,00")
+});
